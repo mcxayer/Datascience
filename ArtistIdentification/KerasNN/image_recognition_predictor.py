f371a532011df8d00a095c3ca90f0a38228534e0
Nousla <sirtheorist@gmail.com>
2017-12-15 13:47:39 +0100

    Added the final nine trainers with accuracies and predictions.

--- ArtistIdentification/KerasNN/image_recognition_predictor.py
+++ ArtistIdentification/KerasNN/image_recognition_predictor.py
@@ -4,12 +4,17 @@ from keras.preprocessing.image import load_img, img_to_array, ImageDataGenerator
 
 dimensions = [64, 64, 3]
 
-model = load_model("cnn_model_v2_500epoch.h5")
+model = load_model("cnn_model_v13_50epoch.h5")
 
 # Manual testing
-image = load_img('./Images/Test/Unknown/image4.png', target_size=(dimensions[0], dimensions[1]))
-image_array = img_to_array(image)
-image_array *= 1./255
-image_array = image_array.reshape((1,) + image_array.shape)
+def test(path):
+    image = load_img(path, target_size=(dimensions[0], dimensions[1]))
+    image_array = img_to_array(image)
+    image_array *= 1. / 255
+    image_array = image_array.reshape((1,) + image_array.shape)
+    print(model.predict(image_array))
 
-print(model.predict(image_array))
+test('./Images/Test/Unknown/image1.png')
+test('./Images/Test/Unknown/image2.png')
+test('./Images/Test/Unknown/image3.png')
+test('./Images/Test/Unknown/image4.png')
---
+++ ArtistIdentification/KerasNN/image_recognition_trainer10.py
@@ -0,0 +1,76 @@
+# Network 5
+
+from keras import Sequential
+from keras.layers import Conv2D, Activation, MaxPooling2D, Flatten, Dense, Dropout
+from keras.preprocessing.image import ImageDataGenerator
+
+dimensions = [64, 64, 3]
+batch_size = 16
+
+model = Sequential()
+model.add(Conv2D(32, 3, input_shape=(dimensions[0], dimensions[1], dimensions[2]),
+                 data_format="channels_last"))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=2))
+
+model.add(Conv2D(32, 3))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=2))
+
+model.add(Conv2D(64, 3))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=2))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation("relu"))
+model.add(Dropout(0.2))
+
+model.add(Dense(4))
+model.add(Activation("softmax"))
+
+model.compile(loss="categorical_crossentropy",
+              optimizer="rmsprop",
+              metrics=["accuracy"])
+
+train_image_data_generator = ImageDataGenerator(
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2
+)
+
+train_image_data_flow = train_image_data_generator.flow_from_directory(
+    "./Images/Training",
+    target_size=(dimensions[0], dimensions[1]),
+    batch_size=batch_size,
+    class_mode="categorical"
+)
+
+validation_image_data_generator = ImageDataGenerator(
+    rescale=1. / 255
+)
+
+validation_image_data_flow = validation_image_data_generator.flow_from_directory(
+    "./Images/Validation",
+    target_size=(dimensions[0], dimensions[1]),
+    batch_size=batch_size,
+    class_mode="categorical"
+)
+
+sample_amount = 1.361 + 1322 + 116 + 600
+validation_sample_amount = 1021
+epochs = 50
+epoch_steps = sample_amount // batch_size
+validation_steps = validation_sample_amount // batch_size
+
+history = model.fit_generator(
+    train_image_data_flow,
+    steps_per_epoch=epoch_steps,
+    epochs=epochs,
+    validation_data=validation_image_data_flow,
+    validation_steps=validation_steps
+)
+
+print(history.history)
+
+model.save("cnn_model_v10_50epoch.h5")
